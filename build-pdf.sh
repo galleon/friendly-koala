4782970a4fb506b533ef2868c2a8d6c5e3f7a461
ggdupont <ger.dupont@gmail.com>
2021-05-02 22:35:08 +0200

    Revert dummy comments

--- build-pdf.sh
+++ build-pdf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#tlmgr update --self
-#tlmgr install adjustbox babel-german background bidi collectbox csquotes everypage filehook footmisc footnotebackref framed fvextra letltxmacro ly1 mdframed mweights needspace pagecolor sourcecodepro sourcesanspro titling ucharcat ulem unicode-math upquote xecjk xurl zref
+tlmgr update --self
+tlmgr install adjustbox babel-german background bidi collectbox csquotes everypage filehook footmisc footnotebackref framed fvextra letltxmacro ly1 mdframed mweights needspace pagecolor sourcecodepro sourcesanspro titling ucharcat ulem unicode-math upquote xecjk xurl zref
 pandoc \
   --pdf-engine=xelatex \
   --template=./templates/eisvogel.tex \
